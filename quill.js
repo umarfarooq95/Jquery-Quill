237d94951b8577fdb0018e6b883a91296d2f50fe
mrsafi <msafiyullah@gmail.com>
2018-12-08 19:33:47 +0530

    implemented bold italic underline functionality

--- quill.js
+++ quill.js
@@ -35,16 +35,39 @@
 
     function buildQuillEditor(currentEle){
         var formatImg = [
-           "https://cdn4.iconfinder.com/data/icons/proglyphs-editor/512/Bold-512.png",
-           "https://cdn4.iconfinder.com/data/icons/proglyphs-editor/512/Italic-512.png",
-           "http://icons.iconarchive.com/icons/icons8/ios7/256/Editing-Underline-icon.png",
+            {
+                imgUrl:"https://cdn4.iconfinder.com/data/icons/proglyphs-editor/512/Bold-512.png",
+                id:'bold'
+            },
+            {
+                imgUrl: "https://cdn4.iconfinder.com/data/icons/proglyphs-editor/512/Italic-512.png",
+                id:'italic'
+            },
+            {
+                imgUrl: "http://icons.iconarchive.com/icons/icons8/ios7/256/Editing-Underline-icon.png",
+                id:'underline'
+            },
+            {
+                imgUrl: "https://cdn2.iconfinder.com/data/icons/text-editing-1/100/text_editing-12-512.png",
+                id:'justifyleft'
+            },
+            {
+                imgUrl: "https://cdn2.iconfinder.com/data/icons/text-format/64/_Align_Center-512.png",
+                id:'justifycenter'
+            },
+            {
+                imgUrl: "https://cdn2.iconfinder.com/data/icons/text-editing-1/100/text_editing-14-512.png",
+                id:'justifyright'
+            }
         ]
         var formattingWrapper = $('<div></div>').addClass('formatting-wrapper');
         formatImg.forEach(function(img){
-            var box = $('<button></button>').addClass('box');
+            var box = $('<button></button>').addClass('box').attr({
+                id:img.id
+            });
             box[0].onclick= onClickFormatDoc;
             var svg = $('<img/>').attr({
-                src:img,
+                src:img.imgUrl,
                 width: '20px'
             })
             box.append(svg)
@@ -55,6 +78,7 @@
 
     function onClickFormatDoc(){
         var self = $(this)[0]
+        document.execCommand(self.id, false, 'div');
         if(!self.classList.contains('active')){
             $(this).addClass('active')
         }
